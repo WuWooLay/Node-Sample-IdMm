0ceff5b93a3b36cc6406d522f631787dd79ae876
WuWooLay <mamamglay.lp@gmail.com>
2019-02-23 04:43:43 +0630

    First Commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+
 const router = express.Router();
 
 router.get('/login', (req, res) => {
@@ -6,7 +8,34 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/register', (req, res) => {
-    res.render('users/register')
+    res.render('users/register');
+});
+
+router.post('/register', (req, res) => {
+    console.log(req.body);
+    const { name, email, password, confirmpassword } = req.body;
+    const error = [];
+
+    if( password != confirmpassword ) {
+        error.push({text: 'Password Do Not Match'});
+    }
+
+    if( password.length <= 4 ) {
+        error.push({text: 'Password must be AtLeast 4 characters'});
+    }
+
+    if( error.length > 0 ) {
+        res.render('users/register', {
+            name,
+            email,
+            password,
+            confirmpassword,
+            error
+        });
+    } else {
+        res.send('Register Post');
+    }
+
 });
 
 module.exports = router;
