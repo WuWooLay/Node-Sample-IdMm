ca587290e7c815d1d581478ea6461dde15195eb4
WuWooLay <mamamglay.lp@gmail.com>
2019-02-23 02:54:19 +0630

    First Commit

--- app.js
+++ app.js
@@ -2,19 +2,12 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-var methodOverride = require('method-override')
- 
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const port = 5000;
 const app = express();
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
-
-// override with the X-HTTP-Method-Override header in the request
-app.use(methodOverride('X-HTTP-Method-Override'))
 
 // Get Rid Of Warning, Mapping Global Promise 
 mongoose.Promise = global.Promise;
@@ -22,6 +15,30 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/mm_video', {useNewUrlParser: true})
     .then(() => console.log('Connect MongoDb...'))
     .catch((err) => console.error(err));
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+// override with the "_method" header in the request
+app.use(methodOverride('_method'));
+
+// Express Session Middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(flash());
+
+// GlobalVariable like Custom Middlewares
+app.use( (req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+});
 
 // Use HandleBars Engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -67,7 +84,7 @@ app.post('/ideas/add', (req, res) => {
     }
 
     if(error.length > 0) {
-        res.render('add_ideas', {
+        res.render('ideas/add', {
             error,
             title: req.body.idea,
             details: req.body.details
@@ -102,7 +119,29 @@ app.get('/ideas/edit/:id', (req, res) => {
 })
 
 app.put('/ideas/:id', (req, res) => {
-    
+    const _id = req.params.id;
+
+    Idea.findById({_id})
+        .then(idea => {
+            // Get Idea Then We'll be gonna to save
+            idea.title = req.body.idea;
+            idea.details = req.body.details;
+            idea.save()
+                .then( idea => res.redirect('/ideas') )
+                .catch( err => console.error('errSave => ', err))
+        })
+        .catch(err => console.error(err));
+});
+
+app.delete('/ideas/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Idea.remove({_id})
+        .then(idea => {
+             req.flash('success_msg', 'Successfully Removeded');
+             res.redirect('/ideas');
+        })
+        .catch(err => console.error(err));
 });
 
 app.listen(port, () => {
